749f54b472e83bc8fe06f1fb54ac3b1e1be80d29
ili_the_butterfly <iliana.decarufel@gmail.com>
2025-09-18 15:01:53 -0400

    Implemented odrive controller interface

--- odrive_controller_interface/src/odrive_controller_interface.cpp
+++ odrive_controller_interface/src/odrive_controller_interface.cpp
@@ -11,174 +11,77 @@
 #include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/float64.hpp"
 
+#include "odrive_types/srv/clear_error.hpp"
+#include "odrive_types/srv/set_absolute_position.hpp"
+
 #include "odrive_controller_interface/odrive_controller_interface_parameters.hpp"
 
 
 namespace odrive_controller_interface {
 
-class Axis;
-
 using trigger_command = double;
 using uint32_state = double;
 using uint8_state = double;
 
+using BoolMsg = std_msgs::msg::Bool;
+
+using TriggerSrv = std_srvs::srv::Trigger;
+using ClearErrorSrv = odrive_types::srv::ClearError;
+using SetAbsolutePositionSrv = odrive_types::srv::SetAbsolutePosition;
+
+using CallbackReturn = controller_interface::CallbackReturn;
+using MapOfReferencesToCommandInterfaces = std::unordered_map<
+  std::string, std::reference_wrapper<hardware_interface::LoanedCommandInterface>>;
+
 class BetterODriveControllerInterface : public controller_interface::ControllerInterface {
-    using TriggerSrv = std_srvs::srv::Trigger;
-    using BoolMsg = std_msgs::msg::Bool;
 
 public:
   BetterODriveControllerInterface();
 
-  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
-
-  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
-
-  controller_interface::return_type update(
-    const rclcpp::Time & time, const rclcpp::Duration & period) override;
-
+  
   controller_interface::CallbackReturn on_init() override;
-
+  
   controller_interface::CallbackReturn on_configure(
-    const rclcpp_lifecycle::State & previous_state) override;
+      const rclcpp_lifecycle::State & previous_state) override;
+      
+    controller_interface::CallbackReturn on_activate(
+        const rclcpp_lifecycle::State & previous_state) override;
+          
+    controller_interface::CallbackReturn on_deactivate(
+        const rclcpp_lifecycle::State & previous_state) override;
+        
+    controller_interface::CallbackReturn on_cleanup(
+        const rclcpp_lifecycle::State & previous_state) override;
+        
+    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-  controller_interface::CallbackReturn on_activate(
-    const rclcpp_lifecycle::State & previous_state) override;
+    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-  controller_interface::CallbackReturn on_deactivate(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-  controller_interface::CallbackReturn on_cleanup(
-    const rclcpp_lifecycle::State & previous_state) override;
-
-  controller_interface::CallbackReturn on_error(
-    const rclcpp_lifecycle::State & previous_state) override;
-
+    controller_interface::return_type update(
+        const rclcpp::Time & time, const rclcpp::Duration & period) override;
 protected:
-  // Parameters from ROS for diff_drive_controller
-//   std::shared_ptr<ParamListener> param_listener_;
-//   Params params_;
+    MapOfReferencesToCommandInterfaces command_interfaces_map_ = {};
 
-private:
-    std::vector<Axis> axes_;
-};
+    // Parameters from ROS for diff_drive_controller
+    std::shared_ptr<ParamListener> param_listener_;
+    Params params_;
 
-struct Axis {
-    Axis() {}
+    // Subscribers
+    rclcpp::Subscription<BoolMsg>::SharedPtr estop_sub_{};
 
-    // Commands (ros2_control => ODrives)
-    double pos_setpoint_ = 0.0; // [rad]
-    double vel_setpoint_ = 0.0; // [rad/s]
-    double torque_setpoint_ = 0.0; // [Nm]
-    double estop_ = -1.0;
-    trigger_command clear_errors_cmd_ = 0.0;
-    double set_absolute_pos_ = 0.0; // [rad]
-    trigger_command set_absolute_pos_cmd_ = 0.0;
+    // Clients
+    rclcpp::Service<TriggerSrv>::SharedPtr clear_all_errors_srv_{};
+    rclcpp::Service<ClearErrorSrv>::SharedPtr clear_errors_srv_{};
+    rclcpp::Service<SetAbsolutePositionSrv>::SharedPtr set_absolute_position_srv_{};
 
-    // State (ODrives => ros2_control)
-    uint32_state axis_error_ = 0;
-    uint8_state axis_state_ = 0;
-    uint8_state procedure_result_ = 0;
-    uint8_state trajectory_done_flag_ = 0;
-    double pos_estimate_ = NAN; // [rad]
-    double vel_estimate_ = NAN; // [rad/s]
-    double torque_target_ = NAN; // [Nm]
-    double torque_estimate_ = NAN; // [Nm]
-    uint32_state active_errors_ = 0;
-    uint32_state disarm_reason_ = 0;
-    double bus_voltage_ = NAN; // [V]
-    double bus_current_ = NAN; // [A]
-    double electrical_power_ = NAN; // [W]
-    double mechanical_power_ = NAN; // [W]
+    bool estop_ = false;
+    bool clear_all_errors_ = false;
+    std::vector<bool> clear_errors_cmd_ = {};
+    std::vector<double> set_absolute_positions_cmd_ = {};
 
-
-    // Indicates which controller inputs are enabled. This is configured by the
-    // controller that sits on top of this hardware interface. Multiple inputs
-    // can be enabled at the same time, in this case the non-primary inputs are
-    // used as feedforward terms.
-    // This implicitly defines the ODrive's control mode.
-    bool pos_input_enabled_ = false;
-    bool vel_input_enabled_ = false;
-    bool torque_input_enabled_ = false;
-
-    // /**
-    //  * @brief Set the axis state
-    //  * 
-    //  * @param state Requested axis state
-    //  */
-    // void send_axis_state(const ODriveAxisState& state);
-    // /**
-    //  * @brief Set the controller mode 
-    //  * 
-    //  * @param control Requested controller mode
-    //  * @param input Requested input mode
-    //  */
-    // void send_controller_mode(const ODriveControlMode& control, const ODriveInputMode& input);
-    /**
-     * @brief Clear errors
-     * 
-     * @param identify Set to 1 to flash the status led
-     */
-    void send_clear_errors(const uint8_t& identify = 0);
-    /**
-     * @brief Set the input position
-     * 
-     * @param input_pos Setpoint [rad]
-     * @param velocity_feed_forward Feed forward velocity [rad/s]. 0 to use configured max
-     * @param torque_feed_forward Feed forward torque [Nm]. 0 to use configured max
-     */
-    void send_input_pos(const double& input_pos, const double& velocity_feed_forward = 0, const double& torque_feed_forward = 0);
-    /**
-     * @brief Set the input velocity
-     * 
-     * @param velocity Setpoint [rad/s]
-     * @param torque_feed_forward Feed forward torque [Nm]. 0 to use configured max
-     */
-    void send_input_vel(const double& velocity, const double& torque_feed_forward = 0);
-    /**
-     * @brief Set the input torque
-     * 
-     * @param torque Setpoint [Nm]
-     */
-    void send_input_torque(const double& torque);
-    /**
-     * @brief Set the E-Stop state
-     * 
-     * @param estop E-Stop state
-     */
-    void send_estop_state(const bool& estop);
-    /**
-     * @brief Set the absolute position
-     * 
-     * @param position Actual position [rad]
-     */
-    void send_absolute_position(const double& position);
-    /**
-     * @brief Set the velocity and current limits
-     * 
-     * @param velocity_limit Velocity limit [rad/s]
-     * @param current_limit Current limit [A]
-     */
-    void send_limits(const double& velocity_limit, const double& current_limit);
-    /**
-     * @brief Set the trajectory velocity limit
-     * 
-     * @param limit Velocity limit [rad/s]
-     */
-    void send_trajectory_vel_limit(const double& limit);
-    /**
-     * @brief Set the trajectory accel/decel limits
-     * 
-     * @param accel_limit Acceleration limit [rad/s^2]
-     * @param decel_limit Deceleration limit [rad/s^2]
-     */
-    void send_trajectory_accel_limits(const double& accel_limit, const double& decel_limit);
-    /**
-     * @brief Set the trajectory inertia
-     * 
-     * @param inertia Inertia [Nm/(rad/s^2)]
-     */
-    void send_trajectory_inertia(const double& inertia);
-
+    // Joints
+    std::vector<std::string> joint_names_ = {};
+    std::vector<std::string> absolute_joint_names_ = {};
 };
 
 
@@ -186,6 +89,192 @@ struct Axis {
 
 using namespace odrive_controller_interface;
 
+BetterODriveControllerInterface::BetterODriveControllerInterface() {}
 
+controller_interface::CallbackReturn BetterODriveControllerInterface::on_init()
+{
+    try
+    {
+        param_listener_ = std::make_shared<odrive_controller_interface::ParamListener>(get_node());
+        params_ = param_listener_->get_params();
+        return CallbackReturn::SUCCESS;
+    }
+    catch (const std::exception & e)
+    {
+        RCLCPP_ERROR_STREAM(get_node()->get_logger(), "Exception thrown during init stage with message: " << e.what() << std::endl);
+        return CallbackReturn::ERROR;
+    }
+}
+
+controller_interface::CallbackReturn BetterODriveControllerInterface::on_configure(const rclcpp_lifecycle::State &previous_state)
+{
+    try
+    {
+        // Reset vectors
+        joint_names_ = {};
+        absolute_joint_names_ = {};
+        set_absolute_positions_cmd_ = {};
+        clear_errors_cmd_ = {};
+        
+        // Register all joints
+        for (auto joint_name : params_.joint_names) {
+            joint_names_.emplace_back(joint_name);
+        }
+        clear_errors_cmd_.resize(joint_names_.size(), false);
+        
+        // Register absolute joints
+        for (auto joint_name : params_.absolute_position_names) {
+            absolute_joint_names_.emplace_back(joint_name);
+        }
+        set_absolute_positions_cmd_.resize(absolute_joint_names_.size(), NAN);
+
+        
+        estop_sub_ = get_node()->create_subscription<BoolMsg>(
+            "~/estop", rclcpp::SystemDefaultsQoS(),
+            [this](const BoolMsg::SharedPtr msg) { estop_ = msg->data; });
+            
+        clear_all_errors_srv_ = get_node()->create_service<TriggerSrv>(
+            "~/clear_all_errors",
+            [this](const TriggerSrv::Request::SharedPtr,
+                TriggerSrv::Response::SharedPtr) {
+                clear_all_errors_ = true;
+            });
+        
+        clear_errors_srv_ = get_node()->create_service<ClearErrorSrv>(
+            "~/clear_errors",
+            [this](const ClearErrorSrv::Request::SharedPtr request,
+                ClearErrorSrv::Response::SharedPtr response) {
+                    for (auto joint_name : request->joint_names) {
+                        auto match = std::find(joint_names_.begin(), joint_names_.end(), joint_name);
+                        if (match != joint_names_.end()) {
+                            auto index = match - joint_names_.begin();
+                            clear_errors_cmd_[index] = true;
+                        } else {
+                            RCLCPP_WARN_STREAM(get_node()->get_logger(), "No joint named '" << joint_name << "'");
+                        }
+                    }
+                });
+        
+        set_absolute_position_srv_ = get_node()->create_service<SetAbsolutePositionSrv>(
+            "~/set_absolute_positions",
+            [this](const SetAbsolutePositionSrv::Request::SharedPtr request,
+                SetAbsolutePositionSrv::Response::SharedPtr response) {
+                    if (request->joint_names.size() != request->positions.size()) {
+                        RCLCPP_ERROR(get_node()->get_logger(), "Size mismatch in 'SetAbsolutePositionSrv'");
+                        return;
+                    }
+                    for (auto joint_name : request->joint_names) {
+                        auto match = std::find(absolute_joint_names_.begin(), absolute_joint_names_.end(), joint_name);
+                        if (match != absolute_joint_names_.end()) {
+                            auto index = match - absolute_joint_names_.begin();
+                            set_absolute_positions_cmd_[index] = request->positions[index];
+                        } else {
+                            RCLCPP_WARN_STREAM(get_node()->get_logger(), "No joint named '" << joint_name << "'");
+                        }
+                    }
+                });
+        
+        RCLCPP_INFO(get_node()->get_logger(), "configure successful");
+        return CallbackReturn::SUCCESS;
+    }
+    catch (const std::exception & e)
+    {
+        RCLCPP_ERROR_STREAM(get_node()->get_logger(), "Exception thrown during configure stage with message: " << e.what());
+        return CallbackReturn::ERROR;
+    }
+}
+
+controller_interface::CallbackReturn BetterODriveControllerInterface::on_activate(const rclcpp_lifecycle::State &previous_state)
+{
+    for (auto joint_name : joint_names_) {
+        {
+            auto match = std::find_if(command_interfaces_.begin(), command_interfaces_.end(),
+                [&](hardware_interface::LoanedCommandInterface& interface) {
+                    return interface.get_prefix_name() == joint_name && interface.get_interface_name() == "estop";
+                }
+            );
+            if (match != command_interfaces_.end()) {
+                command_interfaces_map_.emplace(joint_name + "/estop", std::ref(*match));
+            }
+        }
+
+        {
+            auto match = std::find_if(command_interfaces_.begin(), command_interfaces_.end(),
+                [&](hardware_interface::LoanedCommandInterface& interface) {
+                    return interface.get_prefix_name() == joint_name && interface.get_interface_name() == "clear_error_cmd";
+                }
+            );
+            if (match != command_interfaces_.end()) {
+                command_interfaces_map_.emplace(joint_name + "/clear_error_cmd", std::ref(*match));
+            }
+        }
+    }
+
+    for (auto joint_name : absolute_joint_names_) {
+        {
+            auto match = std::find_if(command_interfaces_.begin(), command_interfaces_.end(),
+                [&](hardware_interface::LoanedCommandInterface& interface) {
+                    return interface.get_prefix_name() == joint_name && interface.get_interface_name() == "set_absolute_position";
+                }
+            );
+            if (match != command_interfaces_.end()) {
+                command_interfaces_map_.emplace(joint_name + "/set_absolute_position", std::ref(*match));
+            }
+        }
+
+        {
+            auto match = std::find_if(command_interfaces_.begin(), command_interfaces_.end(),
+                [&](hardware_interface::LoanedCommandInterface& interface) {
+                    return interface.get_prefix_name() == joint_name && interface.get_interface_name() == "set_absolute_position_cmd";
+                }
+            );
+            if (match != command_interfaces_.end()) {
+                command_interfaces_map_.emplace(joint_name + "/set_absolute_position_cmd", std::ref(*match));
+            }
+        }
+    }
+    return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn BetterODriveControllerInterface::on_deactivate(const rclcpp_lifecycle::State &previous_state)
+{
+    command_interfaces_map_.clear();
+    return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn BetterODriveControllerInterface::on_cleanup(const rclcpp_lifecycle::State &previous_state)
+{
+    return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::InterfaceConfiguration BetterODriveControllerInterface::command_interface_configuration() const
+{
+    auto config = controller_interface::InterfaceConfiguration();
+    config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+    for (auto joint_name : joint_names_) {
+        config.names.emplace_back(joint_name + "/estop");
+        config.names.emplace_back(joint_name + "/clear_errors_cmd");
+    }
+
+    for (auto joint_name : absolute_joint_names_) {
+        config.names.emplace_back(joint_name + "/set_absolute_pos");
+        config.names.emplace_back(joint_name + "/set_absolute_pos_cmd");
+    }
+    return config;
+}
+
+controller_interface::InterfaceConfiguration BetterODriveControllerInterface::state_interface_configuration() const
+{
+    auto config = controller_interface::InterfaceConfiguration();
+    config.type = controller_interface::interface_configuration_type::NONE;
+    return config;
+}
+
+controller_interface::return_type BetterODriveControllerInterface::update(const rclcpp::Time &time, const rclcpp::Duration &period)
+{
+    return controller_interface::return_type();
+}
 
 PLUGINLIB_EXPORT_CLASS(odrive_controller_interface::BetterODriveControllerInterface, controller_interface::ControllerInterface)
+    
